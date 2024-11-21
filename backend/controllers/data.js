ae5bcdc654e866c657a785836b3ff4f2318bce2d
Stelios Taliadwros <steliosevoix@gmail.com>
2024-11-21 14:06:17 +0200

    reaf: response include only the data from the selected date

--- backend/controllers/data.js
+++ backend/controllers/data.js
@@ -1,11 +1,13 @@
 const fetchCvsData = require("../utils/convertToJson");
+const selectedDate = require("../utils/getDataByDay");
 
 async function getData(req, res) {
   try {
-    const data = await fetchCvsData("data/eurofxref.csv");
-    console.log("data: ", data);
+    const data = await fetchCvsData("data/eurofxref-hist.csv");
 
-    res.status(200).json({ success: true, data });
+    const selectedDay = selectedDate(data, req.query.date);
+
+    res.status(200).json({ success: true, selectedDay });
   } catch (error) {
     console.error("Error reading CSV file:", error);
     res.status(500).json({ success: false, error: "Internal server error" });
