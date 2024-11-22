681e123aa30980399e59d3bafc6a407b54513846
Stelios Taliadwros <steliosevoix@gmail.com>
2024-11-22 14:36:43 +0200

    chore: bug fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,44 +3,111 @@ import Header from "./components/Header";
 import CustomInput from "./components/CustomInput";
 
 const App = () => {
+  const [selectedDay, setSelectedDay] = useState("");
   const [from, setFrom] = useState("");
-  const [to, setTo] = useState("1.0583");
+  const [to, setTo] = useState("");
   const [data, setData] = useState([]);
 
   useEffect(() => {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth();
+    const day = today.getDate();
+
+    const fullDateToday = `${year}-${month}-${day}`;
+    setSelectedDay(fullDateToday);
+  }, []);
+
+  useEffect(() => {
     async function fetchData() {
-      const res = await fetch("http://localhost:3000/api/v1/data");
+      const res = await fetch(
+        `http://localhost:3000/api/v1/data?date=${selectedDay}`
+      );
 
       const resData = await res.json();
 
-      setData(resData.data);
-
-      console.log("resData: ", resData);
+      setData(resData.selectedDay);
     }
 
-    fetchData();
-  }, []);
+    if (selectedDay) {
+      setData([]);
+      fetchData();
+    }
+  }, [selectedDay]);
+
+  useEffect(() => {
+    if (data.length > 0) {
+      const firstCurrencyValue = Object.values(data[0])[0];
+      setTo(firstCurrencyValue);
+    } else {
+      setFrom("");
+    }
+  }, [data]);
 
   return (
     <div>
       <Header />
-      <div style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
-        <h3>From Euro</h3>
-      </div>
 
-      <div>
-        <input
-          type="text"
-          value={from}
-          onChange={(e) => setFrom(Number(e.target.value))}
-        />
-      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          gap: "1rem",
+        }}
+      >
+        <div>
+          <h3>From Euro</h3>
+        </div>
+        <div>
+          <input
+            type="date"
+            value={selectedDay}
+            onChange={(e) => setSelectedDay(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            value={from}
+            onChange={(e) => setFrom(e.target.value)}
+            disabled={!data.length || !selectedDay || isNaN(to)}
+            style={{
+              borderRadius: "1.2rem",
+              border: selectedDay && data.length > 0 && "1px solid black",
+              width: "200px",
+              height: "25px",
+              cursor:
+                (!selectedDay || !data.length || isNaN(to)) && "not-allowed",
+              paddingLeft: "1rem",
+            }}
+          />
+        </div>
 
-      <div style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
-        <h3>To </h3>
-        <CustomInput data={data} onValue={to} onChange={setTo} />
+        {!data.length && selectedDay && (
+          <p>{`No available data for ${selectedDay} day, Please try other day`}</p>
+        )}
+
+        <div style={{ display: "flex", alignContent: "center", gap: "1rem" }}>
+          {data.length > 0 && (
+            <>
+              <h3>To</h3>
+              <CustomInput data={data} onChange={setTo} />
+            </>
+          )}
+        </div>
+
+        {isNaN(to) && data.length > 0 ? (
+          <p>No available price for the currency</p>
+        ) : (
+          <p>
+            {data.length > 0 &&
+              selectedDay &&
+              (Number(to) * Number(from)).toFixed(2)}
+          </p>
+        )}
       </div>
-      <p>{(to * from).toFixed(2)}</p>
     </div>
   );
 };
--- frontend/src/components/CustomInput.jsx
+++ frontend/src/components/CustomInput.jsx
@@ -1,13 +1,17 @@
-const CustomInput = ({ data, onValue, onChange }) => {
+const CustomInput = ({ data, onChange }) => {
+  let currencyCode, currencyValue;
+
   return (
-    <select value={onValue} onChange={(e) => onChange(e.target.value)}>
-      {data.map(({ data }, index) =>
-        Object.entries(data).map((key) => (
-          <option key={`${index}-${key}`} value={key[1]}>
-            {key[0]}
+    <select value={currencyValue} onChange={(e) => onChange(e.target.value)}>
+      {data.map((currency) => {
+        currencyCode = Object.keys(currency)[0];
+        currencyValue = currency[currencyCode];
+        return (
+          <option key={currencyCode} value={currencyValue} id={currencyCode}>
+            {currencyCode}
           </option>
-        ))
-      )}
+        );
+      })}
     </select>
   );
 };
